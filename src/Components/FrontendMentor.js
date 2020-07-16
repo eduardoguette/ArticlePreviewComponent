889cc26e1654b40e56431a23b6cb1268e69c00f6
Eduardo Guette <eduardoguette@github.com>
2020-07-16 17:58:34 +0200

    done mobile

--- src/Components/FrontendMentor.js
+++ src/Components/FrontendMentor.js
@@ -3,8 +3,8 @@ import "./FrontendMentor.css"
 function FrontendMentorFoo() {
   return (
     <div className="attribution">
-     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
-      Coded by <a href="/">Eduardo Guette</a>.
+     Challenge by <a target="_blanck" href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
+      Coded by <a target="_blanck" href="https://twitter.com/eduardoguette">Eduardo Guette</a>.
     </div>
   )
 }
