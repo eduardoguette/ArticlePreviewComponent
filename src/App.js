03c414da9e3b7a380a41fc498ded44c44a900728
Eduardo Guette <eduardoguette@github.com>
2020-07-18 00:29:27 +0200

    donee

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Fragment } from 'react';
-import Footer from "./Components/Footer"
-import FrontendMentorFoo from "../src/Components/FrontendMentor"
-import CardComponent from './Components/CardComponent';
-import './App.css';
-import './Components/Responsive.css'
-
-
-
+import React, { Fragment } from "react";
+import Footer from "./Components/Footer";
+import FrontendMentorFoo from "../src/Components/FrontendMentor";
+import CardComponent from "./Components/CardComponent";
+import "./App.css";
+import "./Components/Responsive.css";
 
 function App() {
-
   return (
     <Fragment>
       <CardComponent />
       <FrontendMentorFoo />
-      <Footer />
+      {/* <Footer /> */}
     </Fragment>
   );
 }
--- src/Components/FrontendMentor.js
+++ src/Components/FrontendMentor.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import "./FrontendMentor.css"
+import React from "react";
+import "./FrontendMentor.css";
 function FrontendMentorFoo() {
   return (
     <div className="attribution">
-     Challenge by <a target="_blanck" href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
-      Coded by <a target="_blanck" href="https://twitter.com/eduardoguette">Eduardo Guette</a>.
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. Coded by <a href="https://twitter.com/eduardoguette">Eduardo Guette</a>.
     </div>
-  )
+  );
 }
 
 export default FrontendMentorFoo;
